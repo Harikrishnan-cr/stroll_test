93f83406801fc8c27045af62b86a2d92e86d4fde
Harikrishnan-cr <harikrishnancr1725@gmail.com>
2024-03-12 20:44:06 +0530

    common colors added

---
+++ lib/core/color_const.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StrollColors {
+  static const primaryColor = Color(0xFF0F1115);
+  static const secondaryColor = Color(0xFF232A2E);
+  static const lightWhite = Color.fromARGB(0xCF, 0xCB, 0xFF, 0xFF);
+  static const blackColor = Colors.black;
+  static const whiteColor = Colors.white;
+  static const secondaryWhiteColor = Colors.white;
+}
